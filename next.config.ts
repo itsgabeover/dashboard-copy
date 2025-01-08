8f0c714c71b8a157ea2def2bedf2d540a90b55d6
BoboCman <bobchwalk@outlook.com>
2025-01-08 16:20:47 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   experimental: {
-    serverActions: true
+    serverActions: {
+      bodySizeLimit: '2mb'  // Or whatever limit you need
+    }
   },
   webpack: (config: any) => {
     config.externals = [...config.externals, 'chrome-aws-lambda'];
