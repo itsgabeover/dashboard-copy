1b685804ab50deee7c4341d28055d7975cdc9e64
BoboCman <bobchwalk@outlook.com>
2025-01-17 16:32:57 -0500

    Create page.tsx

---
+++ app/upload/success/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { UploadSuccess } from '@/components/upload-success'
+
+export default function UploadSuccessPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-[#F8FAFC] to-[#E2E8F0] py-16 px-4">
+      <UploadSuccess />
+    </div>
+  )
+}
