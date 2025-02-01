4a9269d318c1ab48bb60bd6b1a725742c2a4ce7d
BoboCman <bobchwalk@outlook.com>
2025-02-01 12:15:07 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+}
+
+module.exports = nextConfig 
