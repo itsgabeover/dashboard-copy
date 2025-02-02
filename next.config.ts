e5341aca8eac23add0367a8901b30c3d54f86896
BoboCman <bobchwalk@outlook.com>
2025-02-02 12:46:30 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next"
-import type { Configuration as WebpackConfig } from "webpack"
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config: WebpackConfig) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     if (config.resolve && config.resolve.alias) {
       config.resolve.alias.canvas = false
     }
