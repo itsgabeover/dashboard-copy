744159f7fc536d189a26640549f5945d9b6b0dc4
BoboCman <bobchwalk@outlook.com>
2025-02-03 18:00:06 -0500

    Delete app/portal  directory

--- app/portal /loading.tsx
+++
@@ -1,9 +0,0 @@
-// app/portal/loading.tsx
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="text-lg">Loading...</div>
-    </div>
-  )
-}
-
