48dba0c1370af8c59c2f43e79118188755166e7f
BoboCman <bobchwalk@outlook.com>
2025-02-04 16:39:19 -0500

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -57,14 +57,7 @@ export default function Dashboard() {
           </div>
         </div>
 
-        <PolicyOverview
-          productName={policyData.data.productName}
-          issuer={policyData.data.issuer}
-          productType={policyData.data.productType}
-          deathBenefit={policyData.data.deathBenefit}
-          annualPremium={policyData.data.annualPremium}
-          riders={policyData.data.riders}
-        />
+        <PolicyOverview {...policyData.data.policyOverview} />
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           <div className="lg:col-span-2">
